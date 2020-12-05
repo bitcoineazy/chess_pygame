9b9a67bfe51c83a6861b22bbe69b0d46f53569b9
bitcoin <bitcoineazy@gmail.com>
2020-12-05 06:15:10 +0300

    legal moves

--- training.py
+++ training.py
@@ -1,17 +1,34 @@
 import chess
-import chess.svg
+#import chess.svg
 
 #print(chess.svg.piece(chess.Piece.from_symbol("R")))
 board = chess.Board()
-board.turn
-bq = len(board.pieces(chess.KING, chess.BLACK))
-#chess.KING.piece_symbol()
-print(chess.Move(chess.A2, chess.A3).uci()) #(chess.A2, chess.A3)
-chess.parse_square('a6')
-chess.SQUARE_NAMES.index('a6')
-new = chess.Move.from_uci("a2a3")
-board.push(new)
+print(board.turn)
+print(board.fullmove_number)
+
+move = chess.Move.from_uci("g1f3")
+move1 = chess.Move.from_uci('a7a6')
+
 print(board.legal_moves)
+print(move in board.legal_moves)
+if move in board.legal_moves:
+    board.push(move)
+if move1 in board.legal_moves:
+    board.push(move1)
 print(board)
-print(board.is_legal(new))
+print(board.legal_moves)
+print(board.is_legal(move1))
 print(board.is_valid())
+print(board.fullmove_number)
+print(board.turn)
+'''
+>>> board = chess.Board()
+>>> board.legal_moves.count()
+20
+>>> bool(board.legal_moves)
+True
+>>> move = chess.Move.from_uci("g1f3")
+>>> move in board.legal_moves
+True
+'''
+
